f98b0276f328b264c5d2a0d3a6d490b149537c61
KristinaMihajlenko <34263871+KristinaMihajlenko@users.noreply.github.com>
2021-12-21 14:41:27 +0300

    Update par_bfs.h

--- par_bfs.h
+++ par_bfs.h
@@ -17,15 +17,12 @@ inline pasl::pctl::parray<int32_t> par_bfs(
     pasl::pctl::parray<int32_t> frontier = {static_cast<int32_t>(start_node)};
     while (frontier.size() != 0)
     {
-        pasl::pctl::parray<uint32_t> curr_nodes_edges_size(
-                frontier.size(),
-                [&frontier, &edges](uint32_t node_idx)
+        pasl::pctl::parray<uint32_t> curr_nodes_edges_size(frontier.size(), [&frontier, &edges](uint32_t node_idx)
                 {
                     return edges[frontier[node_idx]].size();
                 }
         );
-        pasl::pctl::parray<uint32_t> pref_sum = pasl::pctl::scan(
-                curr_nodes_edges_size.begin(), curr_nodes_edges_size.end(), static_cast<uint32_t>(0),
+        pasl::pctl::parray<uint32_t> pref_sum = pasl::pctl::scan(curr_nodes_edges_size.begin(), curr_nodes_edges_size.end(), static_cast<uint32_t>(0),
                 [](uint32_t a, uint32_t b)
                 {
                     return a + b;
@@ -34,9 +31,7 @@ inline pasl::pctl::parray<int32_t> par_bfs(
         );
         uint32_t new_frontier_size = pref_sum[pref_sum.size() - 1] + curr_nodes_edges_size[curr_nodes_edges_size.size() - 1];
         pasl::pctl::parray<int32_t> new_frontier(new_frontier_size, -1);
-        pasl::pctl::parallel_for(
-                static_cast<uint32_t>(0), static_cast<uint32_t>(frontier.size()),
-                [&edges, &frontier, &dist, &new_frontier, &pref_sum](uint32_t node_idx)
+        pasl::pctl::parallel_for(static_cast<uint32_t>(0), static_cast<uint32_t>(frontier.size()), [&edges, &frontier, &dist, &new_frontier, &pref_sum](uint32_t node_idx)
                 {
                     uint32_t from_node = frontier[node_idx];
                     uint32_t start_idx = pref_sum[node_idx];
@@ -54,9 +49,7 @@ inline pasl::pctl::parray<int32_t> par_bfs(
                     }
                 }
         );
-        frontier = pasl::pctl::filter(
-                new_frontier.begin(), new_frontier.end(),
-                [](int32_t cur_node)
+        frontier = pasl::pctl::filter(new_frontier.begin(), new_frontier.end(), [](int32_t cur_node)
                 {
                     return cur_node != -1;
                 }
